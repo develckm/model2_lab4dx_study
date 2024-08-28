4554063ea1d6ebac32722113c1b7917c34c1b7a9
develckm <develckm@gmail.com>
2024-08-28 11:32:56 +0900

    컨트롤 완성

---
+++ src/main/java/com/lab4dx/l06_mvc/controller/CustomerListController.java
@@ -0,0 +1,32 @@
+package com.lab4dx.l06_mvc.controller;
+
+import com.lab4dx.l06_mvc.dto.CustomerDto;
+import com.lab4dx.l06_mvc.service.CustomerService;
+import com.lab4dx.l06_mvc.service.CustomerServiceImp;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/customerList.do")
+public class CustomerListController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<CustomerDto> customers=null;
+
+        try {
+            CustomerService customerService=new CustomerServiceImp();
+            customers=customerService.getAll();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        resp.getWriter().print(customers); //불러왔나 확인
+
+    }
+}
--- src/main/java/com/lab4dx/l06_mvc/service/CustomerServiceImp.java
+++ src/main/java/com/lab4dx/l06_mvc/service/CustomerServiceImp.java
@@ -23,16 +23,34 @@ public class CustomerServiceImp implements CustomerService {
 
     @Override
     public List<CustomerDto> getAll() throws Exception {
-        return List.of();
+        return customerDao.findAll();
     }
 
     @Override
     public CustomerDto get(int id) throws Exception {
-        return null;
+        return customerDao.findById(id);
     }
-
     @Override
     public int register(CustomerDto customerDto) throws Exception {
+        try {
+            conn.setAutoCommit(false);
+            conn.commit(); //세이브 포인트
+            //대출
+            //1.은행잔고확인
+            //2.은행금고 돈을 업데이트
+            //3.업데트 내역 등록
+            //4.대출하는 은행에 금고 업데이트
+            //5.대출받는 고객의 잔고에 금액을 업데이트
+            //6.완료 기록
+            //-> 트랜잭션 (일괄성 == 원자성) : 이중하나라도 오류가 생기면 모두를 취소
+            conn.commit();
+        } catch (Exception e) {
+            conn.rollback();
+            throw new RuntimeException(e);
+        }
+
+
+
         return 0;
     }
 
